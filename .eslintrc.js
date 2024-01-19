1eacc8bc117a3dffddc89b9e5a526f3742e8c94f
Victor Araujo <victormanuel.araujo@outlook.com>
2024-01-18 23:56:08 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+
+  rules: {
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-useless-fragment': 0,
+    'import/no-unresolved': 0,
+  },
 };
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+export const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <></>
+      </NavigationContainer>
+    </>
+  );
+};
